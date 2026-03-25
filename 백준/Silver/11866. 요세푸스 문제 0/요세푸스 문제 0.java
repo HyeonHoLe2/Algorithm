a8bb04a42e945991d0e1802463a7a7883f4dff95
HyeonHoLe2 <108398605+HyeonHoLe2@users.noreply.github.com>
2026-03-26 03:22:03 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 216 ms, Memory: 18660 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        
+        List<Integer> list = new LinkedList<>();
+        
+        for(int i =1;i<=n;i++){
+            list.add(i);
+        }
+        
+        int point = 0;
+        int[] arr = new int[n]; 
+        
+        for(int i =0;i<n;i++){
+            
+            point = (point + k - 1) % list.size();
+            
+            arr[i] = list.remove(point);
+        }
+        
+        System.out.print("<");
+        for(int i=0; i<n;i++){
+            if(i==n-1){
+                System.out.print(arr[i]);
+            } else {
+                System.out.print(arr[i] + ", ");
+            }
+        }
+        System.out.print(">");
+    }
+}
